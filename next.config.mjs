70a7b477bd1ac8986bb8adab3b36481eb18e9b02
harmanpreet07 <harmansidhu2056@gmail.com>
2025-03-31 15:53:55 +0530

    Fist commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      serverComponentsHmrCache: false, // defaults to true
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "rxxqvzzhgekacpvvxjmh.supabase.co",
+        },
+      ],
+    },
+  
+    async headers() {
+      return [
+        {
+          source: "/embed",
+          headers: [
+            {
+              key: "Content-Security-Policy",
+              value: "frame-src 'self' https://wheelify-waitlist.created.app",
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
