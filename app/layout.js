e3dddb30ae3a52c01690ac980483b24eb502840b
Harmanpreet Singh <harmansidhu2056@gmail.com>
2025-07-25 07:53:18 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
               <p>Made with 💗 by Harmanpreet Singh</p>
-            </div>
+            </div> //all done with this
           </footer>
         </body>
       </html>
